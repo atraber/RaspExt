9fc7dcd5f33b1b037dc9513cb4c54ef2cc3a8416
atraber <andytraber@gmail.com>
2013-05-02 21:58:07 +0000

    shutdown script tweaked

--- tools/shutdown_python_script.py
+++ tools/shutdown_python_script.py
@@ -1,16 +1,14 @@
 import RPi.GPIO as GPIO
 import time
+import os
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(7, GPIO.IN)
-var=0
 x=0
-while (var==0) :
-    if GPIO.input(7):
-        x=int(x)+1
-        time.sleep(0.25)
-        print ("pressed")
-    if (x%2==0):
-        time.sleep(0.1)
-    else:
-        raise SystemExit
+while True:
+	if GPIO.input(7):
+		os.system("sudo shutdown -h now")
+		print ("pressed")
+		break
+	else:
+		time.sleep(0.1)
